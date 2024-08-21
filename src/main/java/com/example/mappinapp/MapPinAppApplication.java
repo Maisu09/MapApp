4bce0c8d908682c75dbf13ca66eb19f33b3959d4
Maisu09 <mflorian90@yahoo.com>
2024-08-21 15:03:43 +0300

    First Commit

---
+++ src/main/java/com/example/mappinapp/MapPinAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mappinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapPinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapPinAppApplication.class, args);
+    }
+
+}
